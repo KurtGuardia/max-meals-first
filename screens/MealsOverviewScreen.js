20d71c7b002d4048c1f0bb03d5014ba92ee2a7d4
Kurt Guardia <kaguardiagamarra@wideagency.com>
2023-06-22 12:29:52 +0200

    added dynamic values to screens for titles

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -4,14 +4,28 @@ import {
   Text,
   View,
 } from 'react-native'
-import { MEALS } from '../data/dummy-data'
+import { CATEGORIES, MEALS } from '../data/dummy-data'
 import MealItem from '../components/MealItem'
+import { useLayoutEffect } from 'react'
 
-export default function MealsOverview({ route }) {
+export default function MealsOverview({
+  route,
+  navigation,
+}) {
   const catId = route.params.categoryId
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0
   })
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId,
+    ).title
+
+    navigation.setOptions({
+      title: categoryTitle,
+    })
+  }, [catId, navigation])
 
   function renderMealItem(itemData) {
     const item = itemData.item
